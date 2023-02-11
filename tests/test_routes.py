625fde2919050cec893a093bd087313a1530edde
Anthony Herrera <aherrera@chiang.ec>
2023-02-11 12:51:23 -0500

    list accounts

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -137,3 +137,28 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_all_accounts(self):
+        """It should list all Accounts"""
+        account = self._create_accounts(1)[0]
+        account = self._create_accounts(2)[0]
+        resp = self.client.get(
+            f"{BASE_URL}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 3)
+
+    def test_get_all_accounts_empty(self):
+        """It should list all Accounts empty"""
+        resp = self.client.get(
+            f"{BASE_URL}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 0)
+        
+
+
+
+
